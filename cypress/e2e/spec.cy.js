f33df1a00acee446a9233f77ab532d6be5fd53e7
zvonimirplivelic <zvonimirplivelic91@gmail.com>
2024-02-13 14:18:22 +0100

    refactoring and adding parameters to tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -42,16 +42,16 @@ describe('Visit Cinnamon services website', () => {
 
 // Query for the Projects Navigation link 
 
-describe('Find Cinnamon Projects link', () => {
-  it('finds Cinnamon Projects navigation link"', () => {
+describe('Find Projects link', () => {
+  it('finds Projects navigation link"', () => {
     cy.visit('https://cinnamon.services')
 
-    cy.contains('Projects')
+    cy.get('.e62olcv2').contains('Projects')
   })
 })
 
-describe('Click Cinnamon Projects link', () => {
-  it('clicks the Cinnamon "Projects" link', () => {
+describe('Click Projects fixed Navigation link', () => {
+  it('clicks the fixed navigation Projects link', () => {
     cy.visit('https://cinnamon.services/')
 
     cy.get('.e62olcv2').contains("Projects").click({force: true} )
@@ -59,110 +59,158 @@ describe('Click Cinnamon Projects link', () => {
   })
 })
 
-// Query for the Services Navigation link 
+describe('Click Projects bottom Navigation link', () => {
+  it('clicks the bottom navigation "Projects" link', () => {
+    cy.visit('https://cinnamon.services/')
 
-describe('Find Cinnamon Services link', () => {
-  it('finds Cinnamon Services navigation link"', () => {
-    cy.visit('https://cinnamon.services')
-
-    cy.contains('Services')
+    cy.get('.e62olcv0').contains("Projects").click({force: true} )
+    cy.url().should('include', '/projects')
   })
 })
 
-describe('Click Cinnamon Services link', () => {
-  it('clicks the Cinnamon "Services" link', () => {
+// Query for the Services Navigation link 
+
+describe('Find Services link', () => {
+  it('finds Services navigation link"', () => {
+    cy.visit('https://cinnamon.services')
+
+    cy.get('.e62olcv2').contains('Services')
+  })
+})
+
+describe('Click Services fixed navigation link', () => {
+  it('clicks the fixed navigation "Services" link', () => {
     cy.visit('https://cinnamon.services/')
     
-
     cy.get('.e62olcv2').contains("Services").click({force: true} )
+    cy.url().should('include', '/services')
+  })
+})
+
+describe('Click Services bottom navigation link', () => {
+  it('clicks the bottom navigation "Services" link', () => {
+    cy.visit('https://cinnamon.services/')
+    
+    cy.get('.e62olcv0').contains("Services").click({force: true} )
     cy.url().should('include', '/services')
   })
 })
 
 // Query for the About Us Navigation link 
 
-describe('Find Cinnamon About Us link', () => {
-  it('finds Cinnamon About Us navigation link"', () => {
+describe('Find About Us link', () => {
+  it('finds About Us navigation link"', () => {
     cy.visit('https://cinnamon.services')
 
-    cy.contains('About Us')
+    cy.get('.e62olcv2').contains('About Us')
   })
 })
 
-describe('Click Cinnamon About Us link', () => {
-  it('clicks the Cinnamon "About Us" link', () => {
+describe('Click About Us fixed navigation link', () => {
+  it('clicks the fixed navigation "About Us" link', () => {
     cy.visit('https://cinnamon.services/')
 
     cy.get('.e62olcv2').contains("About Us").click({force: true} )
     cy.url().should('include', '/about')
+  })
+})
 
+describe('Click About Us bottom navigation link', () => {
+  it('clicks the bottom navigation "About Us" link', () => {
+    cy.visit('https://cinnamon.services/')
+
+    cy.get('.e62olcv0').contains("About Us").click({force: true} )
+    cy.url().should('include', '/about')
   })
 })
 
 // Query for the Careers Navigation link 
 
-describe('Find Cinnamon Careers link', () => {
-  it('finds Cinnamon Projects navigation link"', () => {
+describe('Find Careers link', () => {
+  it('finds Careers navigation link"', () => {
     cy.visit('https://cinnamon.services')
 
-    cy.contains('Careers')
+    cy.get('.e62olcv2').contains('Careers')
   })
 })
 
-describe('Click Cinnamon Careers link', () => {
-  it('clicks the Cinnamon "Careers" link', () => {
+describe('Click Careers fixed navigation link', () => {
+  it('clicks the fixed navigation "Careers" link', () => {
     cy.visit('https://cinnamon.services/')
 
     cy.get('.e62olcv2').contains("Careers").click({force: true} )
     cy.url().should('include', '/careers')
+  })
+})
 
+describe('Click Careers bottom navigation link', () => {
+  it('clicks the bottom navigation "Careers" link', () => {
+    cy.visit('https://cinnamon.services/')
+
+    cy.get('.e62olcv0').contains("Careers").click({force: true} )
+    cy.url().should('include', '/careers')
   })
 })
 
 // Query for the Blog Navigation link 
 
-describe('Find Cinnamon Blog link', () => {
-  it('finds Cinnamon Blog navigation link"', () => {
+describe('Find Blog link', () => {
+  it('finds Blog navigation link"', () => {
     cy.visit('https://cinnamon.services')
 
-    cy.contains('Blog')
+    cy.get('.e62olcv2').contains('Blog')
   })
 })
 
-describe('Click Cinnamon Blog link', () => {
-  it('clicks the Cinnamon "Blog" link', () => {
+describe('Click Blog fixed navigation link', () => {
+  it('clicks the fixed navigation "Blog" link', () => {
     cy.visit('https://cinnamon.services/')
 
     cy.get('.e62olcv2').contains("Blog").click({force: true} )
     cy.url().should('include', '/blog')
-
   })
 })
 
-// Query for the Contact Us Navigation link 
+describe('Click Blog bottom navigationlink', () => {
+  it('clicks the "Blog" link', () => {
+    cy.visit('https://cinnamon.services/')
 
-describe('Find Cinnamon Contact Us link', () => {
-  it('finds Cinnamon Contact Us navigation link"', () => {
+    cy.get('.e62olcv0').contains("Blog").click({force: true} )
+    cy.url().should('include', '/blog')
+  })
+})
+
+// Click the Play button
+describe('Click Play button in fixed navigation', () => {
+  it('clicks Play button in fixed navigation"', () => {
     cy.visit('https://cinnamon.services')
 
-    cy.contains('Contact Us')
+    cy.get('.eqnon3u1').click({force: true})
+  })
+})
+// Query for the Contact Us Navigation link 
+
+describe('Find Contact Us link', () => {
+  it('finds Contact Us navigation link"', () => {
+    cy.visit('https://cinnamon.services')
+
+    cy.get('.e12xm54s5').contains('Contact Us')
   })
 })
 
-describe('Click Cinnamon Contact Us link', () => {
-  it('clicks the Cinnamon "Contact Us" link', () => {
+describe('Click Contact Us link', () => {
+  it('clicks the "Contact Us" link', () => {
     cy.visit('https://cinnamon.services/')
 
     cy.get('.e12xm54s5').contains("Contact Us").click({force: true} )
     cy.url().should('include', '/contact')
-
   })
 })
 
 // Enter Subscription e-mail via clicking the Subscribe button (getting by Subscribe text on button)
 
 describe('Subscribe to newsletter', () => {
-  it('finds subscribe form, fills the email credentials and clicks subscribe button successfully', () => {
+  it('finds subscribe form, fills the email credentials and clicks subscribe button', () => {
     cy.visit('https://cinnamon.services/')
 
     // Get an input, type into it
@@ -171,14 +219,14 @@ describe('Subscribe to newsletter', () => {
     //  Verify that the value has been updated
     cy.get('[name="email"]').should('have.value', 'fake@email.com')
 
-    cy.contains('Subscribe').click()
+    cy.get('#subscribeNewsletterButton').contains('Subscribe').click({force: true})
   })
 })
 
 // Does the same but submits the form (getting by form id)
 
 describe('Subscribe to newsletter', () => {
-  it('finds subscribe form, fills the email credentials and submits the form successfully', () => {
+  it('finds subscribe form, fills the email credentials and submits the form', () => {
     cy.visit('https://cinnamon.services/')
 
     cy.get('[name="email"]').type('fake@email.com')
