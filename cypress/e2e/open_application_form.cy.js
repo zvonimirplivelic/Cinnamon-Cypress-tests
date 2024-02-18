58dc8f4e7a894be5622794df41f15f6e09a3b677
zvonimirplivelic <zvonimirplivelic91@gmail.com>
2024-02-18 22:36:44 +0100

    Adding tests for cv and social network data creation and deletion

--- cypress/e2e/open_application_form.cy.js
+++ cypress/e2e/open_application_form.cy.js
@@ -5,43 +5,109 @@ describe('Testing Open application form on Cinnamon website', () => {
     })
 
     it('Fill the Open application form on Cinnamon website with correct credentials', () => {
-        cy.get('[name="FirstName"]').type('FakeFname')
+
+        // First Name
+        cy.get('[name="FirstName"]').type("FakeFname")
         cy.get('[name="FirstName"]').should('have.value', 'FakeFname')
 
+        // Last Name
         cy.get('[name="LastName"]').type('FakeLname')
         cy.get('[name="LastName"]').should('have.value', 'FakeLname')
         
+        // Phone Number
         cy.get('[name="PhoneNumber"]').type('+385991234567')
         cy.get('[name="PhoneNumber"]').should('have.value', '+385991234567')
 
+        // E-mail
         cy.get('[name="Email"]').type('fake@email.com')
         cy.get('[name="Email"]').should('have.value', 'fake@email.com')
-        
-        cy.get('[name="SocialLinks"]').type('fakesociallink.com')
-        cy.get('[name="SocialLinks"]').should('have.value', 'fakesociallink.com')
-        
-        cy.get('select').first().within(() => {
-            cy.contains('Github')
-        }).select('github').should('have.value', 'github')
-        cy.get('select').eq(0).contains('Github')
 
+        // Ad location dropdown
         cy.get('select').eq(1).within(() => {
             cy.contains('Cinnamon website')
         }).select('cinnamonwebsite').should('have.value', 'cinnamonwebsite')
         cy.get('select').last().contains('Cinnamon website')
 
+        // Add cv
         cy.get('input[type=file]').first().selectFile('cypress/fixtures/test.pdf', {force: true})
         cy.get('.e1eaey1u3').first().within(() => {
             cy.get('button').should('be.visible')
         })
 
+        // Consent checkbox
         cy.get('.e1wnkp8s3').first().click({force: true})
         cy.get('.e1wnkp8s1').first().should('be.visible')
 
+        // Personal data checkbox
         cy.get('.e1wnkp8s3').last().click({force: true})
         cy.get('.e1wnkp8s1').last().should('be.visible')
 
         cy.get('#formApplyButton').contains('Apply').click({force: true})
         cy.location("pathname").should('contain', '/thank-you/openApplication')
+    })
+
+
+    it('Adding and deleting a social media link', () => {
+                
+        // Social Links
+        cy.get('[name="SocialLinks"]').type('fakesociallink.com')
+        cy.get('[name="SocialLinks"]').should('have.value', 'fakesociallink.com')
+        
+        // Social Dropdown
+        cy.get('select').first().within(() => {
+            cy.contains('Github')
+        }).select('github').should('have.value', 'github')
+        cy.get('select').eq(0).contains('Github')
+
+        cy.get('.ev30vt00').should('not.exist')
+        cy.get('.e18n6vwg0').click({force: true})
+
+        cy.get('.ev30vt00').contains('fakesociallink.com')
+
+        cy.get('.e1eaey1u4').click({force: true})
+        cy.get('.ev30vt00').should('not.exist')
+
+    })
+
+    it('Adding and deleting a cv file', () => {
+
+        cy.get('.e1eaey1u3').should('not.exist')
+        cy.get('input[type=file]').first().selectFile('cypress/fixtures/test.pdf', {force: true})
+
+        cy.get('.e1eaey1u3').first().within(() => {
+            cy.get('.e1eaey1u4').should('exist')
+        })
+
+        cy.get('.e1eaey1u4').click({force: true})
+    })
+
+    it('First name validation', () => {
+
+
+    })
+
+    it('Last name validation', () => {
+
+
+    })
+
+    it('Email validation', () => {
+
+
+    })
+
+    it('Job advert dropdown validation', () => {
+
+
+    })
+
+    it('CV file validation', () => {
+
+
+    })
+
+    it('Terms of service validation', () => {
+
+
     })
 })
